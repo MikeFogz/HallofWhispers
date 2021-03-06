756f5418e8e2dde8d052d4c61cb320ad611f22e1
AZC <albertzcheng@gmail.com>
2021-03-06 13:50:03 -0800

    modified names to make it work

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // After you run it, creates index.html file inside the "Build" folder.
-if(process.env.NODE_ENV === "production"){
+if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
 };
 // This has "/api" because all routes from this route are implied to contain /api/
