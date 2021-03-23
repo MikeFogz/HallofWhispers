9d5f8a5d3e104b39db64bb921935253ba90d4e6a
Cameron Wright <cam.mac.wright@gmail.com>
2021-03-23 01:11:23 -0700

    password helpblock

--- client/src/Components/RegisterForm/RegisterForm.js
+++ client/src/Components/RegisterForm/RegisterForm.js
@@ -44,82 +44,86 @@ const RegisterForm = (props) => {
   return (
     <>
       {/* <div className="page_container"> */}
-        {/* <div className="d-flex justify-content-center h-100"> */}
+      {/* <div className="d-flex justify-content-center h-100"> */}
 
-          {/* start card container */}
-          {/* <div className="user_card"> */}
-            {/* start card logo container */}
-            {/* <div className="d-flex justify-content-center">
+      {/* start card container */}
+      {/* <div className="user_card"> */}
+      {/* start card logo container */}
+      {/* <div className="d-flex justify-content-center">
               <div className="brand_logo_container">
                 <img src="https://cdn.worldvectorlogo.com/logos/dragon-optical-1.svg" className="brand_logo" alt="logo" />
               </div>
             </div> */}
-            {/* end card logo container */}
-            {/* <div className="d-flex justify-content-center form_container"> */}
+      {/* end card logo container */}
+      {/* <div className="d-flex justify-content-center form_container"> */}
 
-              <form onSubmit={submit}>
-                <div className="input-group mb-3">
-                  <div className="input-group-append">
-                    <span className="input-group-text"><i className="fas fa-user"></i></span>
-                  </div>
-                  <input onChange={onChange} type="text" name="email" className="form-control input_user" placeholder="email" />
-                </div>
+      <form onSubmit={submit}>
+        <div className="input-group mb-3">
+          <div className="input-group-append">
+            <span className="input-group-text"><i className="fas fa-user"></i></span>
+          </div>
+          <input onChange={onChange} type="text" name="email" className="form-control input_user" placeholder="email" />
+        </div>
         <label classname="d-flex justify-content-center mt-3">{errorMessage}</label>
 
-                {/* display name start */}
-                <div className="input-group mb-3">
-                  <div className="input-group-append">
-                    <span className="input-group-text"><i className="fas fa-user"></i></span>
-                  </div>
-                  <input onChange={onChange} type="text" name="displayName" className="form-control input_user" placeholder="display name" />
-                </div>
-                {/* display name end */}
+        {/* display name start */}
+        <div className="input-group mb-3">
+          <div className="input-group-append">
+            <span className="input-group-text"><i className="fas fa-user"></i></span>
+          </div>
+          <input onChange={onChange} type="text" name="displayName" className="form-control input_user" placeholder="display name" />
+        </div>
+        {/* display name end */}
 
-                {/* password start */}
-                <div className="input-group mb-2">
-                  <div className="input-group-append">
-                    <span className="input-group-text"><i className="fas fa-key"></i></span>
-                  </div>
-                  <input onChange={onChange} type={passwordToggle} name="password" className="form-control input_pass" placeholder="password" />
-                </div>
-                {/* password end */}
+        {/* password start */}
+        <div className="input-group mb-2">
+          <div className="input-group-append">
+            <span className="input-group-text"><i className="fas fa-key"></i></span>
+          </div>
+          <input onChange={onChange} type={passwordToggle} name="password" className="form-control input_pass" placeholder="password" />
+        </div>
+        {/* password end */}
 
-                {/* password check start */}
-                <div className="input-group mb-2">
-                  <div className="input-group-append">
-                    <span className="input-group-text"><i className="fas fa-key"></i></span>
-                  </div>
-                  <input onChange={onChange} type={passwordToggle} name="passwordCheck" className="form-control input_pass" placeholder="retype password" />
-                </div>
-                {/* password check end */}
+        <div id="passwordHelpBlock" class="form-text">
+          Your password must be 8-20 characters long, contain letters and numbers, and must not contain spaces, special characters, or emoji.
+        </div>
 
-                {/* show password checkbox start */}
-                <div class="form-group">
-                  <div class="custom-control custom-checkbox">
-                    <input type="checkbox" class="custom-control-input" id="customControlInline" onClick={togglePassword} />
-                    <label class="custom-control-label" for="customControlInline">&nbsp; Show Password</label>
-                  </div>
-                </div>
-                {/* show password checkbox end */}
+        {/* password check start */}
+        <div className="input-group mb-2">
+          <div className="input-group-append">
+            <span className="input-group-text"><i className="fas fa-key"></i></span>
+          </div>
+          <input onChange={onChange} type={passwordToggle} name="passwordCheck" className="form-control input_pass" placeholder="retype password" />
+        </div>
+        {/* password check end */}
+
+        {/* show password checkbox start */}
+        <div class="form-group">
+          <div class="custom-control custom-checkbox">
+            <input type="checkbox" class="custom-control-input" id="customControlInline" onClick={togglePassword} />
+            <label class="custom-control-label" for="customControlInline">&nbsp; Show Password</label>
+          </div>
+        </div>
+        {/* show password checkbox end */}
 
 
-                {/* button start */}
-                <div className="d-flex justify-content-center mt-3 login_container">
-                  <button type="submit" name="button" className="btn login_btn">Register</button>
-                </div>
-                {/* button end */}
+        {/* button start */}
+        <div className="d-flex justify-content-center mt-3 login_container">
+          <button type="submit" name="button" className="btn login_btn">Register</button>
+        </div>
+        {/* button end */}
 
         <a onClick={props.toggleForm} className="d-flex justify-content-center mt-3">
-                  {props.formMode === "login" ? "Sign up here" : "Login here"}
-                </a>
-              
-              </form>
+          {props.formMode === "login" ? "Sign up here" : "Login here"}
+        </a>
 
-            {/* </div> */}
+      </form>
 
-          {/* </div> */}
-          {/* end card container */}
-        {/* </div> */}
+      {/* </div> */}
+
+      {/* </div> */}
+      {/* end card container */}
+      {/* </div> */}
       {/* </div> */}
 
     </>
